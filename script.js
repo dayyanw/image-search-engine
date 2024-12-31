2477243908b97781330e405696ac3695061a87fc
Dayyan Wani <dayyanwani765@gmail.com>
2024-12-31 13:04:21 +0530

    initial files of image search engine

---
+++ script.js
@@ -0,0 +1,52 @@
+const accessKey = "ybc1c38jOZ4jHKYtF2z8ZZU5OIqR3Smj2dOfteoPiUM";
+
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+
+let keyword = "searchBox.value";
+let page = 1;
+
+async function searchImages() {
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if(page === 1){
+        searchResult.innerHTML = "";
+    }
+
+    const result = data.results;
+    result.map((results) =>{
+        const image = document.createElement("img");
+        image.src = results.urls.small; 
+        const imageLink = document.createElement("a");
+        imageLink.href = results.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink)
+        
+        
+
+    })
+    showMoreBtn.style.display = "block"
+    
+} 
+
+searchForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    page = 1;
+    searchImages();
+
+})
+showMoreBtn.addEventListener("click", ()=>{
+    page++;
+    searchImages();
+})
